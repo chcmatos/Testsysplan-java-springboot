bae421857fa335d63c254954630cf6356e8fc038
Carlos Matos <chcmatos@hotmail.com>
2021-04-04 23:03:54 -0300

    ...

--- src/main/java/com/sysplan/api/v2/controller/dto/ClientDTO.java
+++ src/main/java/com/sysplan/api/v2/controller/dto/ClientDTO.java
@@ -4,6 +4,9 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Builder
@@ -15,10 +18,13 @@ public final class ClientDTO {
 
     @Getter
     @ApiModelProperty(value= "Client name")
+    @NotNull
     private final String name;
 
     @Getter
     @ApiModelProperty(value= "Client age")
+    @Min(18)
+    @Max(199)
     private final short age;
 
 }
